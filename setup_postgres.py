a85d4b604408bd3b800581073174211006986de3
khan-git-del <ksoomro256@gmail.com>
2025-09-21 01:06:24 +0500

    Create setup_postgres.py

---
+++ setup_postgres.py
@@ -0,0 +1,26 @@
+import psycopg2
+import os
+
+DB_HOST = os.getenv("POSTGRES_HOST", "localhost")
+DB_NAME = os.getenv("POSTGRES_DB", "crawlerdb")
+DB_USER = os.getenv("POSTGRES_USER", "postgres")
+DB_PASS = os.getenv("POSTGRES_PASSWORD", "postgres")
+DB_PORT = os.getenv("POSTGRES_PORT", "5432")
+
+conn = psycopg2.connect(
+    host=DB_HOST, dbname=DB_NAME, user=DB_USER, password=DB_PASS, port=DB_PORT
+)
+cursor = conn.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS repositories (
+    repo_id TEXT PRIMARY KEY,
+    name TEXT,
+    stars INT,
+    last_updated TIMESTAMP
+);
+""")
+
+conn.commit()
+conn.close()
+print("✅ Postgres setup complete.")
